07d8071f4b377f1c7334d798e8014ec6f260a7b7
Roman <49215713+roman-ms@users.noreply.github.com>
2023-01-31 10:56:27 -0600

    Added a message warning that user is not loggd in

--- frontend/src/components/todos-list.js
+++ frontend/src/components/todos-list.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container'; 
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 
 const TodosList = props => {
     const [todos, setTodos] = useState([]);
@@ -23,32 +24,40 @@ const TodosList = props => {
 
     return ( 
         <Container>
-            {todos.map((todo) => { 
-                return (
-                    <Card key={todo.id} className="mb-3"> 
-                        <Card.Body>
-                            <div> 
-                                <Card.Title>{todo.title}</Card.Title>
-                                <Card.Text><b>Memo:</b> {todo.memo}</Card.Text>
-                                <Card.Text>Date created: {todo.created}</Card.Text>
-                            </div>
-                            <Link to={{
-                                pathname: "/todos/" + todo.id, 
-                                state: {
-                                    currentTodo: todo 
-                                }
-                            }}>
-                                <Button variant="outline-info" className="me-2">
-                                    Edit 
+            {props.token == null || props.token === "" ? ( 
+                <Alert variant='warning'>
+                    You are not logged in. Please <Link to={"/login"}>login</Link> to see your todos. 
+                </Alert>
+            ):( 
+            <div>
+                {todos.map((todo) => { 
+                    return (
+                        <Card key={todo.id} className="mb-3"> 
+                            <Card.Body>
+                                <div> 
+                                    <Card.Title>{todo.title}</Card.Title>
+                                    <Card.Text><b>Memo:</b> {todo.memo}</Card.Text>
+                                    <Card.Text>Date created: {todo.created}</Card.Text>
+                                </div>
+                                <Link to={{
+                                    pathname: "/todos/" + todo.id, 
+                                    state: {
+                                        currentTodo: todo 
+                                    }
+                                }}>
+                                    <Button variant="outline-info" className="me-2">
+                                        Edit 
+                                    </Button>
+                                </Link>
+                                <Button variant="outline-danger">
+                                    Delete 
                                 </Button>
-                            </Link>
-                            <Button variant="outline-danger">
-                                Delete 
-                            </Button>
-                        </Card.Body> 
-                    </Card>
-                ) 
-            })}
+                            </Card.Body> 
+                        </Card>
+                    ) 
+                })}
+            </div>
+            )}
         </Container> 
     );
 }
