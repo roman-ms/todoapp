5d68650b1805c51fff1746aa19fd0150a789a1dd
Roman <49215713+roman-ms@users.noreply.github.com>
2023-06-15 14:32:32 -0500

    First commit

--- frontend/src/services/todos.js
+++ frontend/src/services/todos.js
@@ -3,35 +3,35 @@ import axios from 'axios';
 class TodoDataService{
     getAll(token){
         axios.defaults.headers.common["Authorization"] = "Token " + token;
-        return axios.get('https://mshkvroman.pythonanywhere.com/api/todos/');
+        return axios.get('https://localhost:8000/api/todos/');
     }
     
     createTodo(data, token){ 
         axios.defaults.headers.common["Authorization"] = "Token " + token; 
-        return axios.post('https://mshkvroman.pythonanywhere.com/api/todos/', data);
+        return axios.post('https://localhost:8000/api/todos/', data);
     }
 
     updateTodo(id, data, token){ 
         axios.defaults.headers.common["Authorization"] = "Token " + token; 
-        return axios.put(`'https://mshkvroman.pythonanywhere.com/api/todos/'${id}`, data);
+        return axios.put(`'https://localhost:8000/api/todos/'${id}`, data);
     }
 
     deleteTodo(id, token){ 
         axios.defaults.headers.common["Authorization"] = "Token " + token; 
-        return axios.delete(`'https://mshkvroman.pythonanywhere.com/api/todos/'${id}`);
+        return axios.delete(`'https://localhost:8000/api/todos/'${id}`);
     }
 
     completeTodo(id, token){ 
         axios.defaults.headers.common["Authorization"] = "Token " + token;
-        return axios.put(`https://mshkvroman.pythonanywhere.com/api/todos/${id}/complete`); 
+        return axios.put(`https://localhost:8000/api/todos/${id}/complete`); 
     }
 
     login(data){
-        return axios.post('https://mshkvroman.pythonanywhere.com/api/login/', data);
+        return axios.post('https://localhost:8000/api/login/', data);
     }
 
     signup(data){
-        return axios.post('https://mshkvroman.pythonanywhere.com/api/signup/', data);
+        return axios.post('https://localhost:8000/api/signup/', data);
     } 
 }
     
